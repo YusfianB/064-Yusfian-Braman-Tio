917697c9a15b880d600af7f9bab7b7207a0a3581
YusfianB <137400193+YusfianB@users.noreply.github.com>
2023-09-08 10:02:55 +0700

    Add files via upload

---
+++ 064-Yusfian Braman Tio.py
@@ -0,0 +1,15 @@
+#Luas persegi panjang
+panjang = float(input("Masukkan panjang: "))
+lebar = float(input("Masukkan lebar: "))
+
+luas = panjang * lebar
+
+print("Luas persegi panjang:", luas)
+
+#Luas segitiga
+alas = float(input("Masukkan alas: "))
+tinggi = float(input("Masukkan tinggi: "))
+
+luas = 0.5 * alas * tinggi
+
+print("Luas segitiga:", luas)
